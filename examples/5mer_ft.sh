71ee684cff0e9207e7145a8c0ff12441f71b721c
istiakshihab <istiak@pm.me>
2021-05-12 20:56:41 +0600

    example script

--- examples/5mer_ft.sh
+++ examples/5mer_ft.sh
@@ -1,23 +1,22 @@
 export KMER=5
-export MODEL_PATH=/home/mr/Workspace/Team8993/THESIS/DNABERT/models/5-new-12w-0
-export DATA_PATH=/home/mr/Workspace/Team8993/THESIS/DNABERT/Dataset/$KMER
-export OUTPUT_PATH=/home/mr/Workspace/Team8993/THESIS/DNABERT/Output/$KMER
+export MODEL_PATH=../models/5-new-12w-0
+export DATA_PATH=../Dataset/$KMER/start/
+export OUTPUT_PATH=../Output/$KMER/start/
 
 python run_finetune.py \
     --model_type dna \
     --tokenizer_name=dna$KMER \
     --model_name_or_path $MODEL_PATH \
-    --task_name rnaclass \
+    --task_name dnaprom \
     --do_train \
     --do_eval \
     --early_stop 3\
     --data_dir $DATA_PATH \
-    --validation_split 0.1\
     --max_seq_length 508 \
     --per_gpu_eval_batch_size=8   \
     --per_gpu_train_batch_size=8   \
     --learning_rate 2e-4 \
-    --num_train_epochs 10.0 \
+    --num_train_epochs 12.0 \
     --output_dir $OUTPUT_PATH \
     --evaluate_during_training \
     --logging_steps 100 \
